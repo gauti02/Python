804ed3dfc8d368f3a00b7d87ecca46d87b89df74
gauti02 <gauti.d002@gmail.com>
2026-05-01 12:43:28 +0530

    added snack_suggestions.py file

--- 03_conditionals/snack_suggestions.py
+++ 03_conditionals/snack_suggestions.py
@@ -1,4 +1,9 @@
-snack = input("What snack do you want to eat? ")
+snack = input("What snack do you want to eat? ").lower() #converting user input to lowercase for easier comparison
 
 print("You have chosen to eat " + snack)
 print(f"user said: {snack}") #using f-string for string interpolation
+
+if snack == "cookies" or snack == "samosa":
+    print(f"Great choice! we will serve you {snack}")
+else:
+    print(f"Sorry, we don't have {snack} in our menu. Please choose something else.")
